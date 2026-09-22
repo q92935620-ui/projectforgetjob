c233b7240019d48debe9d508b361d46a6da24a14
Kirill Orlov <kirillorlov1137@gmail.com>
2026-09-23 03:44:34 +0400

    all comit

---
+++ src/test/java/org/example/controller/PostControllerTest.java
@@ -0,0 +1,69 @@
+package org.example.controller;
+
+import org.example.dtoobject.PostDto;
+import org.example.entity.Post;
+import org.example.service.PostService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import tools.jackson.databind.ObjectMapper;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(PostController.class)
+@DisplayName("PostController")
+class PostControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @MockitoBean
+    private PostService postService;
+
+    @Test
+    void findAll() throws Exception {
+        when(postService.findAll()).thenReturn(List.of());
+        mockMvc.perform(get("/api/posts"))
+                .andExpect(status().isOk()).andExpect(content().json("[]"));
+        verify(postService).findAll();
+
+
+    }
+
+    @Test
+    void findById() throws Exception{
+        Long id = 1L;
+        PostDto postDto = new PostDto();
+        postDto.setId(id);
+        postDto.setPlace("Moscow");
+        when(postService.findById(anyLong())).thenReturn(postDto);
+        mockMvc.perform(get("/api/posts/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.place").value("Moscow"));
+
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
